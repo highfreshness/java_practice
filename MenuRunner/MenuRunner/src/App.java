50bfe87fc9e96a19ab10a64db325cc42d16debdf
highfreshness <meteo0718@gmail.com>
2023-05-04 11:27:25 +0900

    first commit

---
+++ MenuRunner/MenuRunner/src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc= new Scanner(System.in);
+        // sc.close();
+        System.out.println("Enter Number1:");
+        int number1 = sc.nextInt();
+        System.out.println("The number you entered is - " + number1);
+        
+        System.out.println("Enter Number2 :");
+        int number2 = sc.nextInt();
+        System.out.println("The number you entered is - "+ number2);
+        
+        System.out.println("Choose the operater");
+        System.out.println("1-Add, 2-Subract, 3-Divide, 4-Multiply");
+        int choice = sc.nextInt();
+        performOperationUsingNestedIfElse(number1, number2, choice);
+        
+    }
+
+    private static void performOperationUsingNestedIfElse(int number1, int number2, int choice) {
+        switch(choice) {
+            case 1 : System.out.println("Result : "+ (number1 + number2)); break;
+            case 2 : System.out.println("Result : "+ (number1 - number2)); break;
+            case 3 : System.out.println("Result : "+ (number1 / number2)); break;
+            case 4 : System.out.println("Result : "+ (number1 * number2)); break;
+            default : System.out.println("Invalid Operation"); 
+        }
+    }
+}
