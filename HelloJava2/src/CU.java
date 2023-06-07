493757686288668f35800efbe98dd9a76dd30934
DESKTOP-609PPKH\user <meteo0718@gmail.com>
2023-06-07 17:20:11 +0900

    2023-06-07 commit

---
+++ HelloJava2/src/CU.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CU {
+
+    public String chooseBread() {
+        String bread;
+        // 3. 빵을 고른다.(소보로 - 1000원, 생크림빵 - 1500원)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("둘 중 원하는 빵을 입력해주세요 : soboro or cream");
+        bread = sc.nextLine();
+        return bread;
+    }
+
+    public int changeMoney(String bread, Object left) {
+        int price;
+        if (bread.equals("soboro")){
+            price = 1000;
+        }
+        else{
+            price = 1500;
+        }
+        return (int)left - price;
+    }
+}
